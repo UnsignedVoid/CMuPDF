677c0b050459d13d39acff8b6fdefaaa45c4d89f
Kiryl Matsaberydze <kiryl.matsaberydze@gmail.com>
2024-01-27 20:48:07 +0100

    Initial Commit

---
+++ MuPDF/MuPDF.h
@@ -0,0 +1,18 @@
+//
+//  MuPDF.h
+//  MuPDF
+//
+//  Created by Kiryl Matsaberydze on 27/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MuPDF.
+FOUNDATION_EXPORT double MuPDFVersionNumber;
+
+//! Project version string for MuPDF.
+FOUNDATION_EXPORT const unsigned char MuPDFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuPDF/PublicHeader.h>
+
+
